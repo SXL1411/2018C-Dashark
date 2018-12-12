ed5dc2125b323e62859b253f100820879c906ec9
SXL1411 <2574585075@qq.com>
2018-12-12 16:24:22 +0800

    initit

---
+++ 寻找单身汪.cpp
@@ -0,0 +1,35 @@
+
+
+#include <stdio.h>
+int main()
+{
+	int a[100],n,t;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(int j=1;j<n;j++)
+	{
+		for(int k=0;k<j;k++)
+		{
+			if(a[k]<a[j])
+			{
+				t=a[k];
+				a[k]=a[j];
+				a[j]=t;	
+			}
+		}	
+	}
+	int c=0,d=0;
+	for(int s=0;s<n;s=s+2)
+	{
+		c=c+a[s];
+	}
+	for(int m=1;m<n;m=m+2)
+	{
+		d=d+a[m];
+	}
+	int danshu=c-d;
+	printf("%d",danshu);
+ } 	
